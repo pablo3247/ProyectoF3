d90b33817f8c49ad6eb30668e60d80e46daddb9f
Vicastello24 <vichergon5@alu.edu.gva.es>
2025-05-20 10:09:06 +0200

    Creacion JWT

---
+++ Backend/main/com/ejemplo/aplicacion/JwtLoginSpringbootApplication.java
@@ -0,0 +1,13 @@
+package ejemplo.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@ejemplo.aplicacion.SpringBootApplication
+public class JwtLoginSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtLoginSpringbootApplication.class, args);
+    }
+}
+
---
+++ Backend/main/com/ejemplo/aplicacion/repositorio/RepositorioUsuario.java
@@ -0,0 +1,2 @@
+public class RepositorioUsuario {
+}
