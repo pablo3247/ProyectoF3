cc5e977181fcd8fd0033f7fda738132a66083b03
raquel <raqbenbes@alu.edu.gva.es>
2025-05-20 19:53:28 +0200

    raquel 19/5

--- Backend/main/com/ejemplo/aplicacion/SpringBootApplication.java
+++ Backend/main/com/ejemplo/aplicacion/SpringBootApplication.java
@@ -1,4 +1,12 @@
-package ejemplo.aplicacion;
+package com.ejemplo.aplicacion;
 
-public @interface SpringBootApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLoginSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JwtLoginSpringbootApplication.class, args);
+    }
 }
+
