f0f5e2d8962df32603da88cec5800d7f3110559c
Vicastello24 <vichergon5@alu.edu.gva.es>
2025-05-20 10:50:33 +0200

    todas las clases

--- Backend/main/com/ejemplo/aplicacion/repositorio/RepositorioUsuario.java
+++ Backend/main/com/ejemplo/aplicacion/repositorio/RepositorioUsuario.java
@@ -1,2 +1,9 @@
-public class RepositorioUsuario {
+
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioUsuario extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCorreo(String correo);
+    Optional<Usuario> findByTelefono(String telefono);
 }
