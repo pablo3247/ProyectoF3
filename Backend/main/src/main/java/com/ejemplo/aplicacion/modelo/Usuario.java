5d2005fc30199b0ac745b2fa2845276059678f6e
pablo_casa <pabmoranc@alu.edu.gva.es>
2025-05-21 10:08:10 +0200

    Cambios back

---
+++ Backend/main/src/main/java/com/ejemplo/aplicacion/modelo/Usuario.java
@@ -0,0 +1,45 @@
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String correo;
+    private String telefono;
+    private String contrasenaHash;
+    private String rol; // por ejemplo: "cliente" o "admin"
+    private String nombre;
+    private String apellidos;
+    private String dnnni;
+
+
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public String getContrasenaHash() { return contrasenaHash; }
+    public void setContrasenaHash(String contrasenaHash) { this.contrasenaHash = contrasenaHash; }
+
+    public String getRol() { return rol; }
+    public void setRol(String rol) { this.rol = rol; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellidos() { return apellidos; }
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+
+    public String getDnnni() { return dnnni; }
+    public void setDnnni(String dnnni) { this.dnnni = dnnni; }
+
+}
