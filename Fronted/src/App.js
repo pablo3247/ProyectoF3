2832c2dbb182514eeb308917e76b8d8fb42f82ac
raquel <raqbenbes@alu.edu.gva.es>
2025-05-21 17:22:42 +0200

    raquel 19/5

--- Fronted/src/App.js
+++ Fronted/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FormularioUsuario from "./components/FormularioUsuario";
+import FormularioUsuario from "./FormularioUsuario";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/formulario" />} />
         <Route path="/formulario" element={<FormularioUsuario />} />
       </Routes>
     </Router>
