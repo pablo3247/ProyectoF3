a2753bd5cc72e51762f6b3f9085bf023b81c528f
Pablo <pabmoranc@alu.edu.gva.es>
2025-05-23 11:52:37 +0200

    Delete VersionAnterior"Funcional" directory

--- VersionAnterior"Funcional"/ProyectoF3_ModByPablo/FullStackAPI/Login con el fetch/js/proteger_rutas.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-        window.location.href = "login.html"; // Redirige al login solo si no hay token
-    }
-});
