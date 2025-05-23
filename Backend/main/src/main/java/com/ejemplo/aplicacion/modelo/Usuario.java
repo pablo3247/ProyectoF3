80245cd3603bcdeebd6a87771a2b76befaa457cb
raquel <raqbenbes@alu.edu.gva.es>
2025-05-23 13:00:06 +0200

    23/05

--- Backend/main/src/main/java/com/ejemplo/aplicacion/modelo/Usuario.java
+++ Backend/main/src/main/java/com/ejemplo/aplicacion/modelo/Usuario.java
@@ -8,12 +8,24 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String nombre;
+    private String apellidos;
+    private String dni;
     private String correo;
     private String telefono;
     private String contrasenaHash;
     private String rol; // por ejemplo: "cliente" o "admin"
 
     // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellidos() { return apellidos; }
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+
+    public String getDni() { return dni; }
+    public void setDni(String dni) { this.dni = dni; }
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
