3fcdab0dbfdef61f6aefc086361a421a4829d526
Pablo <pabmoranc@alu.edu.gva.es>
2025-05-27 19:51:55 +0200

    Delete ProyectoFinal_PomArregladoConParent1 directory

--- ProyectoFinal_PomArregladoConParent1/src/main/java/com/ejemplo/aplicacion/repositorio/RepositorioFirma.java
+++
@@ -1,8 +0,0 @@
-
-package com.ejemplo.aplicacion.repositorio;
-
-import com.ejemplo.aplicacion.modelo.Firma;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositorioFirma extends JpaRepository<Firma, Long> {
-}
